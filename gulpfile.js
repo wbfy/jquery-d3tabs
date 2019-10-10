c57c162ac9fa52f4e14dd63f722c3f03d74e0468
Websites Built For You <sp@sp-dev>
2019-10-10 16:31:14 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ * Build project
+ * Requires Gulp 4.0.0
+ */
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass');
+
+gulp.task
+	(
+		'compile.project',
+		function (doneCallBack) {
+			gulp.src(['src/js/*.js'])
+				.pipe(concat('jquery-d3tabs.min.js'))
+				.pipe(uglify())
+				.pipe(gulp.dest('dist'));
+
+			gulp.src(['demo.scss'])
+				.pipe(concat('demo.min.css'))
+				.pipe(sass({ outputStyle: 'compressed', includePaths: ['client/scss'] }))
+				.pipe(gulp.dest('.'));
+
+			doneCallBack();
+		}
+	);
+
+gulp.task('compile', gulp.parallel('compile.project'));
